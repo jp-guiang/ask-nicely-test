4a060c97ae438b6cbc9cad435dd7866c39484cee
JP Guiang <jpguiang15@gmail.com>
2024-07-29 17:43:08 +1200

    feat: show author initials on side

--- src/components/SingleNotification.vue
+++ src/components/SingleNotification.vue
@@ -1,18 +1,31 @@
 <script setup lang="ts">
-import { defineProps } from 'vue'
+import { defineProps, computed } from 'vue'
 import type { PropType } from 'vue'
 import type { NotificationModel } from '@/models/notification-model'
-defineProps({
+
+const props = defineProps({
   notification: {
     type: Object as PropType<NotificationModel>
   }
+})
+
+const initials = computed(() => {
+  let initials = props.notification.author.match(/\b\w/g)?.join('') || ''
+  return initials
 })
 </script>
 
 <template>
   <div class="text-white p-4 flex border-b-2 border-b-white/[.1]">
-    <div class="w-auto p-2">
-      {{ notification.read }}
+    <div class="w-1/12 p-2 flex text-center items-center">
+      <div class="p-2">
+        <div v-if="notification.read" class="p-1 rounded-full bg-white"></div>
+      </div>
+      <div class="flex items-center justify-center">
+        <p>
+          {{ initials }}
+        </p>
+      </div>
     </div>
     <div class="w-full p-2 flex flex-wrap">
       <div class="w-full">
